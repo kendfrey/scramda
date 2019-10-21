21eb2f083f0f01627fb9472a2e2e3d150d1ef01d
Kendall Frey <kendfrey@gmail.com>
2019-10-21 19:21:20 -0400

    Helper operators

---
+++ src/Scramda/DSL.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Scramda.DSL
+  ( (!>)
+  , (!)
+  , a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+  ) where
+
+import Scramda
+
+class IsExpr a where
+  expr :: a -> Expr
+
+instance IsExpr Expr where
+  expr = id
+
+instance IsExpr String where
+  expr = Var
+
+infixr 8 !>
+(!>) :: IsExpr a => String -> a -> Expr
+x !> e = Lam x (expr e)
+
+infixl 9 !
+(!) :: (IsExpr a, IsExpr b) => a -> b -> Expr
+f ! x = App (expr f) (expr x)
+
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = "e"
+f = "f"
+g = "g"
+h = "h"
+i = "i"
+j = "j"
+k = "k"
+l = "l"
+m = "m"
+n = "n"
+o = "o"
+p = "p"
+q = "q"
+r = "r"
+s = "s"
+t = "t"
+u = "u"
+v = "v"
+w = "w"
+x = "x"
+y = "y"
+z = "z"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE BlockArguments #-}
+
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit (assertEqual)
 import Scramda
+import Scramda.DSL
 
 main :: IO ()
 main = defaultMain tests
@@ -9,36 +12,36 @@ main = defaultMain tests
 tests :: [Test]
 tests =
   [ testGroup "Eq"
-    [ testCase "variable" $ do
+    [ testCase "variable" do
         assertEqual "Variable equality failed"
           (Var "x")
           (Var "x")
-    , testCase "lambda" $ do
+    , testCase "lambda" do
         assertEqual "Lambda equality failed"
           (Lam "x" (Var "x"))
           (Lam "x" (Var "x"))
-    , testCase "apply" $ do
+    , testCase "apply" do
         assertEqual "Apply equality failed"
           (App (Var "x") (Var "y"))
           (App (Var "x") (Var "y"))
     ]
 
   , testGroup "Show"
-    [ testCase "variable" $ do
+    [ testCase "variable" do
         assertEqual "Variable show failed"
           "x"
           (show $ Var "x")
-    , testCase "lambda" $ do
+    , testCase "lambda" do
         assertEqual "Lambda show failed"
           "x -> x"
-          (show $ Lam "x" (Var "x"))
-    , testCase "apply" $ do
+          (show $ x !> x)
+    , testCase "apply" do
         assertEqual "Apply show failed"
           "x y"
-          (show $ App (Var "x") (Var "y"))
-    , testCase "combined" $ do
+          (show $ x ! y)
+    , testCase "combined" do
         assertEqual "Combined show failed"
           "(x -> x) (y (z -> z z))"
-          (show $ App (Lam "x" (Var "x")) (App (Var "y") (Lam "z" (App (Var "z") (Var "z")))))
+          (show $ (x !> x) ! (y ! (z !> z ! z)))
     ]
   ]
