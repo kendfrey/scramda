73f7b65b053571b49a8bca015a9efc75f7adad7b
Kendall Frey <kendfrey@gmail.com>
2019-10-21 18:17:18 -0400

    Better Show implementation

--- src/Scramda.hs
+++ src/Scramda.hs
@@ -3,4 +3,15 @@ module Scramda
   ) where
 
 data Expr = Var String | Lam String Expr | App Expr Expr
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Expr where
+  show (Var s) = s
+  show (Lam x e) = x ++ " -> " ++ show e
+  show (App f x) = showF f ++ " " ++ showX x
+    where
+      showF f@(Lam _ _) = "(" ++ show f ++ ")"
+      showF f = show f
+      showX x@(Lam _ _) = "(" ++ show x ++ ")"
+      showX x@(App _ _) = "(" ++ show x ++ ")"
+      showX x = show x
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,44 @@
 import Test.Framework
 import Test.Framework.Providers.HUnit
-import Test.HUnit
+import Test.HUnit (assertEqual)
 import Scramda
 
 main :: IO ()
 main = defaultMain tests
 
+tests :: [Test]
 tests =
   [ testGroup "Eq"
-    [ testCase "variable" $ assertEqual "Variable equality failed" (Var "x") (Var "x")
-    , testCase "lambda" $ assertEqual "Lambda equality failed" (Lam "x" (Var "x")) (Lam "x" (Var "x"))
-    , testCase "apply" $ assertEqual "Apply equality failed" (App (Var "x") (Var "y")) (App (Var "x") (Var "y"))
+    [ testCase "variable" $ do
+        assertEqual "Variable equality failed"
+          (Var "x")
+          (Var "x")
+    , testCase "lambda" $ do
+        assertEqual "Lambda equality failed"
+          (Lam "x" (Var "x"))
+          (Lam "x" (Var "x"))
+    , testCase "apply" $ do
+        assertEqual "Apply equality failed"
+          (App (Var "x") (Var "y"))
+          (App (Var "x") (Var "y"))
     ]
+
   , testGroup "Show"
-    [ testCase "variable" $ assertEqual "Variable show failed" "Var \"x\"" (show $ Var "x")
-    , testCase "lambda" $ assertEqual "Lambda show failed" "Lam \"x\" (Var \"x\")" (show $ Lam "x" (Var "x"))
-    , testCase "apply" $ assertEqual "Apply show failed" "App (Var \"x\") (Var \"y\")" (show $ (App (Var "x") (Var "y")))
+    [ testCase "variable" $ do
+        assertEqual "Variable show failed"
+          "x"
+          (show $ Var "x")
+    , testCase "lambda" $ do
+        assertEqual "Lambda show failed"
+          "x -> x"
+          (show $ Lam "x" (Var "x"))
+    , testCase "apply" $ do
+        assertEqual "Apply show failed"
+          "x y"
+          (show $ App (Var "x") (Var "y"))
+    , testCase "combined" $ do
+        assertEqual "Combined show failed"
+          "(x -> x) (y (z -> z z))"
+          (show $ App (Lam "x" (Var "x")) (App (Var "y") (Lam "z" (App (Var "z") (Var "z")))))
     ]
   ]
